30947c556dd2df81133933c0280673620b4fc94d
Alexandr143 <oleksanderskapa@gmail.com>
2022-05-17 00:25:09 +0300

    fourth task is done

---
+++ goit-js-hw-02/javaScript/task-04.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const formatString = function(string) {
+
+    console.log(string.length);
+
+    if (string.length > 40) {
+
+     return string.slice(0,40) + '...';
+
+    };
+
+    return string;
+
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+  // вернется оригинальная строка
+  
+  console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+  // вернется форматированная строка
+  
+  console.log(formatString('Curabitur ligula sapien.'));
+  // вернется оригинальная строка
+  
+  console.log(
+    formatString(
+      'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+    ),
+  );
+  // вернется форматированная строка
