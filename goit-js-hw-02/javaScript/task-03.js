58af541ce72e506026b1009e048e2061e4005514
Alexandr143 <oleksanderskapa@gmail.com>
2022-05-16 23:29:09 +0300

    third task is done

--- goit-js-hw-02/javaScript/task-03.js
+++ goit-js-hw-02/javaScript/task-03.js
@@ -1,3 +1,37 @@
 'user strict';
 
-console.log('hello');
+const findLongestWord = function(str) {
+    
+    let strSplit = str.split(' ');
+
+    let longestWord = strSplit[0]; 
+
+    // for (let i of strSplit) {
+    //     if(i.length > longestWord.length){
+    //       longestWord = i;
+    //     };
+    // };
+
+    for (let i = 0; i < strSplit.length; i+=1) {
+
+        if(strSplit[i].length > longestWord.length){
+
+            longestWord = strSplit[i];
+
+        };
+
+    };
+
+     return longestWord;
+    
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+  
+  console.log(findLongestWord('Google do a roll')); // 'Google'
+  
+  console.log(findLongestWord('May the force be with you')); // 'force'
+
