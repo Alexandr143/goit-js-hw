44cee3f63c66c4a5f4b75d5ff49e78e082d104fe
Alexandr143 <oleksanderskapa@gmail.com>
2022-05-17 23:43:50 +0300

    fifth task is done

--- goit-js-hw-02/javaScript/task-05.js
+++ goit-js-hw-02/javaScript/task-05.js
@@ -1,2 +1,22 @@
 'user strict';
 
+const SPAM = 'spam'; 
+
+const SALE = 'sale';
+
+const checkForSpam = function(message) { 
+   
+   return message.toLowerCase().includes(SPAM) || message.toLowerCase().includes(SALE);
+
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(checkForSpam('Latest technology news')); // false
+  
+  console.log(checkForSpam('JavaScript weekly newsletter')); // false
+  
+  console.log(checkForSpam('Get best sale offers now!')); // true
+  
+  console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
