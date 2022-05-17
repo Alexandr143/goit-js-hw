24f8a3b6b380788fc1e818fa20ed3cb1ba46ecbf
Alexandr143 <oleksanderskapa@gmail.com>
2022-05-18 01:52:18 +0300

    it should be completed

---
+++ goit-js-hw-02/javaScript/task-06.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let input;
+const numbers = [];
+let total = 0;
+
+while (true) {
+ 
+    input = prompt('Can you please input number?');
+
+    if (input === null) {
+
+        break;
+
+    } 
+
+        numbers.push(Number(input));
+
+};
+
+for (let i = 0; i < numbers.length; i+=1) {
+
+    total +=numbers[i];
+        
+    };
+
+console.log('Total sum of numbers is',total);
